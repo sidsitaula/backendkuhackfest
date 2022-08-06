740e96f987a298c72a4a9fe134e80da5e657b399
Yush Pokharel <github.com/Yush-Pokharel>
2022-08-06 15:28:23 +0545

    profile picture upload

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/authentication");
 const authMiddleware = require("./middlewares/auth.middleware");
 const courseStream = require("./routes/courseStream");
-const courseItem = requite("./routes/courseItem");
+const courseItem = require("./routes/courseItem");
 const notFound = require("./middlewares/notFound");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const user = require("./routes/userPFP.routes");
 
 //creating server instance
 const app = express();
@@ -15,6 +18,13 @@ const app = express();
 
 app.use(express.json());
 //app.use('/',authMiddleware)
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(cors());
 
 //routes
 app.get("/", (req, res) => {
@@ -27,8 +37,10 @@ app.get("/contact", (req, res) => {
 
 //Routers
 //app.use('/',authRoute)
+app.use("/public", express.static("public"));
 app.use("/api/v1/courseStream", courseStream);
 app.use("/api/v1/courseStream/courseItem", courseItem);
+app.use("/api/v1/user", user);
 app.use(notFound);
 
 //configuring env for environment variables
